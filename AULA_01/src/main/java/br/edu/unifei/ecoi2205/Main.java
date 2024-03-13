b1d5b5cef3d1b351f9764223961c280fb4d87214
Walter Aoiama Nagai <walternagai@unifei.edu.br>
2024-03-13 10:22:28 -0300

    Commit with empty classes

--- AULA_01/src/main/java/br/edu/unifei/ecoi2205/Main.java
+++ AULA_01/src/main/java/br/edu/unifei/ecoi2205/Main.java
@@ -6,6 +6,9 @@ import br.edu.unifei.ecoi2205.vehicle.Vehicle;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+            TO-DO:
+         */
         Motor motor = new Motor(4, 100, 50, Fuel.GAS, 50);
         Vehicle vehicle = new Vehicle("Apple", "Silverbullet", 4, 0, motor);
 
