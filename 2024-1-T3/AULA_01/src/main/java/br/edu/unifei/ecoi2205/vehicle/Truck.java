8b954449d127b23350236a8054def2fa168cc58d
Walter Aoiama Nagai <walternagai@unifei.edu.br>
2024-03-17 08:53:45 -0300

    Removidas configurações e classes não utilizadas

---
+++ 2024-1-T3/AULA_01/src/main/java/br/edu/unifei/ecoi2205/vehicle/Truck.java
@@ -0,0 +1,8 @@
+package br.edu.unifei.ecoi2205.vehicle;
+
+public class Truck extends Vehicle {
+    public Truck(String brand, String model, float mileage, Motor motor) {
+        super(brand, model, 10, mileage, motor);
+    }
+}
+
---
+++ 2024-1-T3/AULA_01/src/main/java/br/edu/unifei/ecoi2205/vehicle/Vehicle.java
@@ -0,0 +1,60 @@
+package br.edu.unifei.ecoi2205.vehicle;
+
+public class Vehicle implements IVehicleOperations {
+    private final String brand;
+    private final String model;
+    private final int wheelsNumber;
+    private float mileage;
+    private final Motor motor;
+
+    public Vehicle(String brand, String model, int wheelsNumber, float mileage, Motor motor) {
+        this.brand = brand;
+        this.model = model;
+        this.wheelsNumber = wheelsNumber;
+        this.mileage = mileage;
+        this.motor = motor;
+    }
+
+    @Override
+    public String toString() {
+        return new String("\tBrand\t" + this.brand + "\n" +
+                "\tModel\t" + this.model + "\n" +
+                "\tMilage\t" + this.mileage);
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getWheelsNumber() {
+        return wheelsNumber;
+    }
+
+    public float getMileage() {
+        return mileage;
+    }
+
+    public Motor getMotor() {
+        return motor;
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Starting the motor of " + this.model);
+    }
+
+    @Override
+    public void travelBy(float kilometers) {
+        this.mileage = this.getMileage() + kilometers;
+        System.out.println("Go through " + kilometers + "km");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Stopping the motor of " + this.model);
+    }
+}
